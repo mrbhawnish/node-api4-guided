a8ae728b92f9cc6953bfcfa2e2bd8747fbbb2f88
bhawnishk1 <mrbhawnish@gmail.com>
2020-10-29 10:02:31 -0700

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000; //ONLY LOCAL
+const secret = process.env.SECRET_THING || "the secret for development"
+ // ONLY HEROKU
+console.log(port, secret);
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
